862b6b1544966eee4ca27bccbab8e7f543df3d5f
Andrew Downing <a_downing@hotmail.com>
2021-02-18 22:21:03 +1100

    Working with strings

---
+++ 02_data_types/js.js
@@ -0,0 +1,12 @@
+/* Strings */
+// Store letters or other characters
+// The syntax requires single or double quotes
+let myString = "Hello world"
+console.log(myString)
+
+// Strings can be joined together using concatenation
+// + will concatenate strings together in JS
+let firstName = "Andrew"
+let surname = "Downing"
+let fullName = firstName + " " + surname
+console.log(fullName)
