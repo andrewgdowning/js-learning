78e4d2b7d307c8498d918936d30d07db8feea0bf
Andrew Downing <a_downing@hotmail.com>
2021-06-12 15:17:05 +1000

    learning loops

---
+++ 03_control_flow/js.js
@@ -0,0 +1,14 @@
+// Control flow
+/* Use of conditional statements or loops */
+
+let names = ['Andrew', 'Elle', 'Lauren']
+
+// For loops
+// A for loop allows the execution of a code block for a certain condition
+// let i = 0; initiatives a variable as 0
+// i < 5 is the condition. While i is less than 5 the code will run
+// i++ adds 1 to i each time the code block is run
+for (let i = 0; i < names.length; i++) {
+    const element = names[i];
+    
+}
