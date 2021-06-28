f6386b840b9122fa7fa5a307f92f2cf1187b025d
Andrew Downing <a_downing@hotmail.com>
2021-06-28 21:06:49 +1000

    object literal

---
+++ 05_object_literals/js.js
@@ -0,0 +1,19 @@
+/* Objects 
+Objects have properties and methods
+Properties store information about the instance of that object
+Methods are things that the object can do
+*/
+
+// objects are defined with {}
+// object properties are stored in key value pairs e.g. name: 'Andrew'.
+let user = {
+    name: 'Andrew',
+    age: 30,
+    email: 'test@testest.test',
+    location: 'berlin',
+    blogPosts: ['what to do today', 'what I did yesterday']
+}
+
+
+console.log(user)
+console.log(user.name)
