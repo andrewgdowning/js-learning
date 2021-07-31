9d5ec89b7a989df6ebb21b102b8ebe2e67a72855
Andrew Downing <a_downing@hotmail.com>
2021-07-31 13:17:57 +1000

    quiz project

---
+++ js.js
@@ -0,0 +1,7 @@
+let form = document.querySelector('form');
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    let score = 0;
+    console.log(e)
+    let userAnswers = [form.question-one.value]
+});
